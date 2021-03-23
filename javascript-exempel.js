99874568c216fe5f651ec8d5fd65a84120249d28
ValentinDobrican <72019892+ValentinDobrican@users.noreply.github.com>
2021-03-23 13:53:57 +0100

    .

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -2,8 +2,8 @@ function authorDialog() {
     alert("Valentin har skapat sidan, den 22 mars 2021.");
 }
 function Färg(){
+    document.getElementById("rubrik").innerHTML = "Valentin";
     document.getElementById("demo").style.backgroundColor = "lightblue";
-    document.getElementById("rubrik").innerText = "Valentin";
 }
 /* Valentin
     Datum: 22 mars 2021
