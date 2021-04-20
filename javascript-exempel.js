4eac25c6c1cdb77f4a1cf8a6b40dcfa4925bf7bd
ValentinDobrican <72019892+ValentinDobrican@users.noreply.github.com>
2021-04-20 15:18:48 +0200

    .

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -6,14 +6,19 @@ function Färg(){
     document.getElementById("demo").style.backgroundColor = "lightblue";
 }
 function bild() {
-    if (document.getElementById("myImage").src== "Counter-Strike_Global_Offensive_icon.jpg") {
-        document.getElementById("myImage").src = "Cristiano_Ronaldo_20120609_(1).jpg";
+    if (document.getElementById("myImage").src == "https://live.staticflickr.com/65535/48605819027_8f47be59f2_b.jpg") {
+        document.getElementById("myImage").src = "http://media.steampowered.com/apps/csgo/blog/images/fb_image.png?v=6";
+        document.getElementById("undertext").innerHTML = "Bästa spelet!";
     }
     else {
-        document.getElementById("myImage").src = "Counter-Strike_Global_Offensive_icon.jpg";
+        document.getElementById("myImage").src = "https://live.staticflickr.com/65535/48605819027_8f47be59f2_b.jpg";
+        document.getElementById("undertext").innerHTML = "Skit spel ";    
     }
 }
-
+function skrivtext(){
+    var namn = document.getElementById("namn").value;
+    document.getElementById("text").innerHTML = namn;
+}
 /* Valentin
     Datum: 22 mars 2021
 */
